36831cc60f688980a8cc23a31a00052acccce6f4
Alexander Chernyshev <33011221+AlexanderChernyshev@users.noreply.github.com>
2024-04-02 12:20:03 -0400

    default config with novaterraprime tileset

--- src/gameConfig.js
+++ src/gameConfig.js
@@ -1,6 +1,6 @@
-import terraPrime from "./tiles/terraPrime.js";
+import novaTerraPrime from "./tiles/novaTerraPrime.js";
 
 export const defaultGameConfig = {
-  tileSet: terraPrime,
+  tileSet: novaTerraPrime,
   minTiles: 10,
 };
