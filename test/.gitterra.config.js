b971351be3e7f4b81ef506ebb075d3fbf48a9939
Alexander Chernyshev <33011221+AlexanderChernyshev@users.noreply.github.com>
2024-04-14 18:27:49 -0400

    first pass for using svg tiles

--- test/.gitterra.config.js
+++ test/.gitterra.config.js
@@ -1,16 +1,17 @@
 export default (config) => {
   config.minTiles = 5;
 
-  // config.tileSet = {
-  //   numberOfTileVariations: 11,
-  //   tileOriginalWidth: 200,
-  //   highestTileOriginalHeight: 420,
-  //   getTileImageURL: function (tileNumber) {
-  //     return `https://gitterra.com/images/tiles/terraprime/tiles_v2-${tileNumber
-  //       .toString()
-  //       .padStart(2, "0")}.svg`;
-  //   },
-  // };
+  config.tileSet = {
+    numberOfTileVariations: 2,
+    tileOriginalWidth: 2013,
+    highestTileOriginalHeight: 1774,
+    tileFolder: "src/images/tiles/novaterraprime",
+    // getTileImageURL: function (tileNumber) {
+    //   return `https://gitterra.com/images/tiles/terraprime/tiles_v2-${tileNumber
+    //     .toString()
+    //     .padStart(2, "0")}.svg`;
+    // },
+  };
 
   return config;
 };
