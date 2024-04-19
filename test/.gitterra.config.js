1da634868801dff74e6d33be47da943106a0e813
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2024-04-19 00:17:00 -0400

    Removed unnecessary props from config.

--- test/.gitterra.config.js
+++ test/.gitterra.config.js
@@ -4,9 +4,6 @@ export default (config) => {
   config.minTiles = 5;
 
   // config.tileSet = {
-  //   numberOfTileVariations: 10,
-  //   tileOriginalWidth: 2013,
-  //   highestTileOriginalHeight: 1774,
   //   tileFolder: url.fileURLToPath(import.meta.resolve("./mytiles")),
   // };
 
