d307a4114a293ebe586d5795ae3635639e34e109
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2024-08-07 21:58:22 -0400

    Explicitly set folder to get history on

--- src/generate_history.sh
+++ src/generate_history.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# Desgination folder
-HISTORY_DIR=$1
+# Code directory
+FULL_CODE_DIR="$(cd $1 && pwd)"
+
+# Destination folder
+mkdir -p $2
+FULL_HISTORY_DIR="$(cd $2 && pwd)"
 
 # Get the current script's directory
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 GENERATE_RECORD_SCRIPT="$SCRIPT_DIR/generate_history_record.sh"
 
+cd $FULL_CODE_DIR
+
 # Current branch
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-mkdir -p $HISTORY_DIR
-
 git log --pretty=format:"%H %ad" --date=short | awk '!seen[$2]++' | \
-    xargs -n2 $GENERATE_RECORD_SCRIPT $HISTORY_DIR
+    xargs -n2 $GENERATE_RECORD_SCRIPT $FULL_HISTORY_DIR
 
 git checkout --quiet $BRANCH
